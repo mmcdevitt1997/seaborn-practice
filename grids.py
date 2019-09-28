1530c3834bc61cb2d95e94d32170a628dc993454
mmcdevitt1997 <30372557+mmcdevitt1997@users.noreply.github.com>
2019-09-28 13:54:52 -0500

    grid chapter done

--- grids.py
+++ grids.py
@@ -3,16 +3,20 @@ import numpy as np, pandas as pd; np.random.seed(0)
 import seaborn as sns; sns.set(style="white", color_codes=True)
 
 iris = sns.load_dataset('iris')
-
+tips = sns.load_dataset('tips')
 # print(iris.head())
 
 unique_species=iris['species'].unique()
 
 # print(unique_species)
 
-pair_grid= sns.PairGrid(iris)
+# pair_grid= sns.PairGrid(iris)
 
-pair_grid.map_diag(sns.distplot)
-pair_grid.map_upper(plt.scatter)
-pair_grid.map_lower(sns.kdeplot)
+# pair_grid.map_diag(sns.distplot)
+# pair_grid.map_upper(plt.scatter)
+# pair_grid.map_lower(sns.kdeplot)
+# plt.show()
+
+tips_grid = sns.FacetGrid(data=tips, col='time', row='smoker')
+tips_grid.map(sns.distplot, 'total_bill')
 # plt.show()
